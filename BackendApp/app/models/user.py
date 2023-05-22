54b4cd9c30a0edf2f61a1a1ecda17f1b2e31896a
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-05-22 21:15:47 +0200

    created user model

---
+++ BackendApp/app/models/user.py
@@ -0,0 +1,7 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(255))
