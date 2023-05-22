4fad19039693d72f0eddde9cd323ac622108ca93
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-05-22 23:24:07 +0200

    editing user model

--- BackendApp/app/models/user.py
+++ BackendApp/app/models/user.py
@@ -3,5 +3,6 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), unique=True)
+    user_name = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(255))
