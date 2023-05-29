f412032c164813b7c565059f888af100a7f9e518
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-05-29 22:59:56 +0200

    created register component

--- FrontendApp/src/app/app.module.ts
+++ FrontendApp/src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './components/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  //{ path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' }, // Redirect to register page by default
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
