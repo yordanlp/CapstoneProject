e7b36b20e59f58f8dea8833a9ef237c66c307d19
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-05-30 23:15:49 +0200

    wip

--- BackendApp/app/models/app_models/generic_response.py
+++ BackendApp/app/models/app_models/generic_response.py
@@ -1,10 +1,14 @@
 from ...utils import object_to_dict
 
 class GenericResponse:
-    def __init__( self, data = None, errors = None ):
+    def __init__( self, data = None, errors = None, code = 200 ):
         self.data = None
         self.errors = None
         self.success = False
+        self.code = code
+
+        if code == 500:
+            errors = ["Something wrong happend, contact the administrator if it persists"]
         
         if errors == None:
             self.success = True
