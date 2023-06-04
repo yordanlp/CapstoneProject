a8020da26b0dd0e7f2ff04499f56e965142463db
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-06-05 01:08:51 +0200

    layout of images in home component

--- FrontendApp/src/app/components/home/home.component.ts
+++ FrontendApp/src/app/components/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ImageService } from 'src/app/services/image.service';
+import { AppConfig } from 'src/app/services/app-config.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  imagesObservable$: Observable<any> = null!;
+  imagesSubscription: Subscription = null!;
+
+  constructor(private imageService: ImageService) {
+    this.imagesObservable$ = this.imageService.getImages();
+
+  }
+  ngOnDestroy(): void {
+    this.imagesSubscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    this.imagesSubscription = this.imagesObservable$.subscribe(
+      data => console.log(data)
+      , error => console.error(error)
+    );
+  }
+
+
 
 }
