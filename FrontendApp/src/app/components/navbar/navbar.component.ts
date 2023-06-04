8420034337451f8519d4abc7df48bb8bbae22b04
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-06-04 20:18:55 +0200

    created navbar component

---
+++ FrontendApp/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isOpen: boolean = false;
+  dropdownOpen: boolean = false;
+
+  constructor(public userService: UserService) { }
+
+  toggleNavbar(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  closeDropdown(): void {
+    this.dropdownOpen = false;
+  }
+}
