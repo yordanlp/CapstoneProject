b632b6a7f73ec3370c7d4856a9b13b82d7cd55ee
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-06-05 01:09:04 +0200

    created image card component

---
+++ FrontendApp/src/app/components/image-card/image-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ImageService } from 'src/app/services/image.service';
+
+@Component({
+  selector: 'app-image-card',
+  templateUrl: './image-card.component.html',
+  styleUrls: ['./image-card.component.css']
+})
+export class ImageCardComponent implements OnInit {
+
+  @Input() imageData: any
+  imageBlob$: Observable<any> = null!
+  imageSubscription: Subscription = null!;
+  blobUrl: string = ""
+
+  constructor(private imageService: ImageService) {
+
+  }
+  ngOnInit(): void {
+    this.imageBlob$ = this.imageService.getImage(this.imageData.id);
+    this.imageSubscription = this.imageBlob$.subscribe(data => {
+      this.blobUrl = URL.createObjectURL(data);
+      console.log(this.blobUrl);
+    });
+  }
+}
