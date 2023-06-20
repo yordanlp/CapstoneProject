4b7cccfcd32667f6f79f28973916940688b3fee1
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-06-21 00:36:17 +0200

    modify alis config to use Shared folder volume to store data

--- Jobs/Alis/config.py
+++ Jobs/Alis/config.py
@@ -5,9 +5,9 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 class Config:
     DEBUG = False
     ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-    UPLOAD_FOLDER = f"{ROOT_PATH}/images"
-    OUTPUT_PROJECTION = f"{ROOT_PATH}/projection"
-    OUTPUT_PCA = f"{ROOT_PATH}/pca"
+    UPLOAD_FOLDER = f"{ROOT_PATH}/Shared/images"
+    OUTPUT_PROJECTION = f"{ROOT_PATH}/Shared/projection"
+    OUTPUT_PCA = f"{ROOT_PATH}/Shared/pca"
 
 class ProductionConfig(Config):
     pass
