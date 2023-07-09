b071ecb6bfad28191dad5544e74936e21799a8b9
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-07-09 20:00:37 +0200

    Update api.py

--- Jobs/Alis/api.py
+++ Jobs/Alis/api.py
@@ -4,7 +4,6 @@ alis_path = os.path.abspath('./alis')
 sys.path.append(alis_path)
 
 from flask import Flask, jsonify, abort, request
-from werkzeug.utils import secure_filename
 import secrets
 
 from Alis import AlisWrapper
@@ -16,54 +15,28 @@ import pickle as pkl
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    try:
-        if 'file' not in request.files:
-            abort(500, 'Missping file parameter in rquest')
-
-        file = request.files['file']
-        if file is None:
-            return jsonify({
-                'success': False,
-                'message': 'You must upload a file'
-                })
-        
-        random_hex = secrets.token_hex(8)
-        filename = random_hex + secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return jsonify({
-            'success': True,
-            'message': 'Image uploaded successfully',
-            'image_id': filename
-        })
-    except PermissionError:
-        return jsonify({
-            'success': False,
-            'message': 'Error: permission denied'
-        })
-
-
 @app.route('/random_images', methods=['GET'])
 def random_images():
+    params = request.get_json()
     try:
-        image_count = int(request.args.get('image_count'))
+        images_names = params['images_names']
     except:
         return jsonify({
             'success' : False,
             'message' : 'Incorrect data format'
         })
     
+    image_count = len(images_names)    
     if image_count > 10:
         return jsonify({
             'success': False,
             'message': 'You can generate up to 10 images'   
         })
-    
+
     w_list, img_list = AlisWrapper.get_random_images(n_samples=image_count)
     img_names, w_names = [], []
     for i in range(image_count):
-        filename = secrets.token_hex(8)
+        filename = images_names[i]
         PIL.Image.fromarray(img_list[i], 'RGB').save(os.path.join(app.config['UPLOAD_FOLDER'], filename + '.png'))
         with open(os.path.join(app.config['OUTPUT_PROJECTION'], filename + '.pkl'), 'wb') as f:
             pkl.dump(w_list[i], f)
@@ -71,6 +44,8 @@ def random_images():
         w_names.append(filename + '.pkl')        
     
     return jsonify({
+        'success': True,
+        'message': 'Images satisfactory generated',
         'images' : img_names,
         'latents' : w_names
     })
@@ -97,7 +72,7 @@ def run_projection():
     file_name = image_id.split('.')[0]
     return jsonify({
         'success' : True,
-        'message': 'Projection run successfully',
+        'message': 'Projection ran successfully',
         'vector_id': f"{file_name}.pkl"
     })
         
@@ -107,12 +82,18 @@ def run_pca():
     vector_id = params['vector_id']
     latent_edits = [( i['principal_component_number'], (i['start_layer'], i['end_layer']), (i['lower_coeff_limit'], i['upper_coeff_limit']) ) for i in params['latent_edits']]
     if vector_id is None:
-        abort(404, 'Vector not found')
+        return jsonify({
+            'success': False,
+            'message': 'Vector not found in request'
+        })
 
     vector_path = f"{app.config['OUTPUT_PROJECTION']}/{vector_id}"
 
     if not os.path.exists(vector_path):
-        abort(404, 'Vector not found')
+        return jsonify({
+            'success': False,
+            'message': f'Vector {vector_path} not found'
+        })
 
     AlisWrapper.run_pca_moving_on_file(
         file_path = vector_path,
@@ -123,7 +104,7 @@ def run_pca():
 
     return jsonify({
         'success' : True,
-        'message': 'PCA run successfully'
+        'message': 'PCA ran successfully'
     })
 
 if __name__ == '__main__':
