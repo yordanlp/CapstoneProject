d692abbf458d7f7a0673b8c649d9c89304ef5ec4
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-07-23 20:47:17 +0200

    feat: add models config

--- Jobs/Stylegan2/config.py
+++ Jobs/Stylegan2/config.py
@@ -2,6 +2,40 @@ import os
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 
+models_config = {
+    'cars': {
+        'checkpoint': 'https://nvlabs-fi-cdn.nvidia.com/stylegan2/networks/stylegan2-car-config-f.pkl',
+        'layers': 16,
+        'resolution': 512,
+    },
+    'faces': {
+        'checkpoint': 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl',
+        'layers': 18,
+        'resolution': 1024,
+    },
+    'cats': {
+        'checkpoint': 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/afhqcat.pkl',
+        'layers': 16,
+        'resolution': 512,
+    },
+    'dogs': {
+        'checkpoint': 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/afhqdog.pkl',
+        'layers': 16,
+        'resolution': 512,
+    },
+    'wild': {
+        'checkpoint': 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/afhqwild.pkl',
+        'layers': 16,
+        'resolution': 512,
+    },
+    'churches': {
+        'checkpoint': 'https://nvlabs-fi-cdn.nvidia.com/stylegan2/networks/stylegan2-church-config-f.pkl',
+        'layers': 14,
+        'resolution': 256,
+    },
+}
+
+
 class Config:
     DEBUG = False
     ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
