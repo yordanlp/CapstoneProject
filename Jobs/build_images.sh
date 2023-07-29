cc270e33f4402a376be754cd7ae1b55b6b596830
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-07-29 14:33:42 +0200

    refactor: adjust build script to build new env containers

--- Jobs/build_images.sh
+++ Jobs/build_images.sh
@@ -1,4 +1,9 @@
 docker build -f BaseCuda.dockerfile -t base_cuda .
+
+docker build -f Alis/environment.dockerfile -t alis_env Alis/.
 docker build -f Alis/Dockerfile -t alis Alis/.
+
+docker build -f Stylegan2/environment.dockerfile -t stylegan2_env Stylegan2/.
 docker build -f Stylegan2/Dockerfile -t stylegan2 Stylegan2/.
+
 docker build -f Worker/Dockerfile -t worker Worker/.
